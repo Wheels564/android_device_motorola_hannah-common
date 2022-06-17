24803de876bfefee254189c6a9467121b1f37465
Jabiyeff <cebiyevanar@gmail.com>
2022-06-17 10:32:07 +0400

    msm8937-common: prop: Force disable iorapd
    * Its eating more RAM and slow down device
    * Thanks for CAFEBABE find this
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -10,3 +10,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.soc.manufacturer=QTI \
     ro.soc.model=MSM8937
+
+# App launch prefetching (IORapd)
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.iorapd.enable=false \
+iorapd.perfetto.enable=false \
+iorapd.readahead.enable=false \
+persist.device_config.runtime_native_boot.iorap_readahead_enable=false
